c5ca752f4cdb3e9e2173c95f4367dd083681ec74
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2021-04-02 17:52:45 +0200

    new docker image

--- src/Sharpbox.Demo/Program.cs
+++ src/Sharpbox.Demo/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Channels;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Sharpbox.Demo;
 
 var services = new ServiceCollection();
